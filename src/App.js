f8727fe9874481d2581ce6672cbc223bdc9266e3
Charus <chalisa4824@gmail.com>
2026-07-20 11:26:36 +0700

    Create class-components.js
    create class-components.js with calender

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Content, Footer, Header } from './func-components';
-
+import Calendar from './class-components';
 function App() {
   
   return (
     <>
       <Header />
+      <p><center><Calendar /></center></p>
       <Content />
       <Footer />
-    </>
+   </>
   )
 }
 
---
+++ src/class-components.js
@@ -0,0 +1,19 @@
+import React,{Component} from 'react'
+
+export default class Calendar extends Component {
+  getDate(){
+    const dayNames =['อาทิตย์','จันทร์','อังคาร','พุธ','พฤหัสบดี','ศุกร์','เสาร์']
+    const monthNames =['มกราคม','กุมภาพันธ์','มีนาคม','เมษายน','พฤษภาคม','มิถุนายน','กรกฎาคม','สิงหาคม','กันยายน','ตุลาคม','พฤศจิกายน','ธันวาคม']
+    const date = new Date()
+    const day = date.getDate()
+    const weekDay = dayNames[date.getDay()]
+    const month = monthNames[date.getMonth()]
+    const year = date.getFullYear() + 543
+    return `วัน${weekDay}ที่ ${day} ${month} ${year}`
+  }
+
+  render(){
+    return <div style={{textAlign:'center'}}>{this.getDate()}</div>
+  }
+ }
+
